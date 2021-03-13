0b44b0896ccb1edcc438e7b614f0025d7702c202
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-03-13 17:24:42 +0000

    First commit

---
+++ CookCLI/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  SPI
+//
+//  Created by Alexey Dubovskoy on 21/12/2020.
+//  Copyright © 2020 Alexey Dubovskoy. All rights reserved.
+//
+
+import Foundation
+import CookInterpreter
+
+if CommandLine.arguments.count == 2 {
+    let recipe = try String(contentsOfFile: CommandLine.arguments[1], encoding: String.Encoding.utf8)
+    let parser = Parser(recipe)
+    let node = parser.parse()
+    let analyzer = SemanticAnalyzer()
+    let parsed = analyzer.analyze(node: node)
+    
+    print(CommandLine.arguments[1].replacingOccurrences(of: "\\.cook$", with: "", options: [.regularExpression]))
+    print("============================")
+    print("Ingredients")
+    print(parsed.ingredientsTable)
+    print("============================")
+    print("Steps")
+    
+    for (index, step) in parsed.steps.enumerated() {
+        print("\(index): \(step)")
+    }    
+} else {
+    print("Usage: SPI prograssm.pas")
+}
