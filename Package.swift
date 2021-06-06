6ef8b65c35226d68fc6f0a134e4f61615c84e68e
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-06-06 19:25:59 +0100

    Server which compiles statically

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CookCLI",
     platforms: [
-        .macOS(.v10_15)        
+        .macOS(.v10_15)
     ],
     products: [
         .executable(name: "cook", targets: ["CookCLI"])
@@ -16,26 +16,23 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/cooklang/CookInSwift", from: "1.2.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-//        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-//        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/envoy/Embassy.git", from: "4.0.0"),
+        .package(url: "https://github.com/envoy/Ambassador.git", from: "4.0.0"),
     ],
     targets: [
-//        .target(
-//            name: "App",
-//            dependencies: [
-//                .product(name: "Leaf", package: "leaf"),
-//                .product(name: "Vapor", package: "vapor"),
-//            ],
-//            swiftSettings: [
-//                // Enable better optimizations when building in Release configuration. Despite the use of
-//                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-//                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-//                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-//            ]
-//        ),
+        .target(
+            name: "App",
+            dependencies: [.product(name: "Embassy", package: "Embassy"), .product(name: "Ambassador", package: "Ambassador")],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
         .target(
             name: "CookCLI",
-            dependencies: ["CookInSwift", .product(name: "ArgumentParser", package: "swift-argument-parser")]), // .target(name: "App"),
+            dependencies: ["CookInSwift", .target(name: "App"), .product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "CookCLITests",
             dependencies: ["CookCLI"]),
--- Sources/App/server.swift
+++ Sources/App/server.swift
@@ -5,14 +5,30 @@
 //  Created by Alexey Dubovskoy on 27/05/2021.
 //
 
-import Vapor
+import Embassy
+import Ambassador
+import Foundation
 
 public func startServer() throws {
-    var env = Environment(name: "development", arguments: ["cook"])
-    try LoggingSystem.bootstrap(from: &env)
-    print(env)
-    let app = Application(env)
-    defer { app.shutdown() }
-    try configure(app)
-    try app.run()
+
+
+
+    let loop = try! SelectorEventLoop(selector: try! SelectSelector())
+    let router = Router()
+    let server = DefaultHTTPServer(eventLoop: loop, interface: "::", port: 9080, app: router.app)
+
+    router["/api/v2/users"] = DelayResponse(JSONResponse(handler: { _ -> Any in
+        return [
+            ["id": "01", "name": "john"],
+            ["id": "02", "name": "tom"]
+        ]
+    }))
+
+    // Start HTTP server to listen on the port
+    try! server.start()
+
+    print("started server on http://localhost:9080")
+    // Run event loop
+    loop.runForever()
+
 }
