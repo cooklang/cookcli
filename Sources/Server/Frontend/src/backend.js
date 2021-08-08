3638091c547f5e9461d20cb8991b76bbb2bc33f4
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2021-08-08 15:57:28 +0100

    Serve from /

--- Sources/Server/Frontend/src/backend.js
+++ Sources/Server/Frontend/src/backend.js
@@ -1,19 +1,17 @@
-const HOST = "http://localhost:9080";
-
 export const fetchFileTree = (async () => {
-    const response = await fetch(`${HOST}/api/v1/file_tree`);
+    const response = await fetch(`/api/v1/file_tree`);
     return await response.json();
 })
 
 
 export const fetchRecipe = (async (path) => {
-    const response = await fetch(`${HOST}/api/v1/recipe/${path}`);
+    const response = await fetch(`/api/v1/recipe/${path}`);
     return await response.json();
 })
 
 
 export const fetchShoppingList = (async (recipes) => {
-    const response = await fetch(`${HOST}/api/v1/shopping-list`,{
+    const response = await fetch(`/api/v1/shopping-list`,{
         method: "POST",
         headers: {
             "Accept": "application/json",
