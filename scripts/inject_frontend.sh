d97b4c0e013ab3e2b1547076a9e54b8afe8b677a
Jake Bell <sigterm01@gmail.com>
2021-10-29 11:14:40 -0500

    Allowing frontend build script to be run from any location

--- scripts/inject_frontend.sh
+++ scripts/inject_frontend.sh
@@ -1,7 +1,7 @@
 # run to inject static assets into code
 # bash ./inject_frontend.sh
 
-cd Sources/Server/Frontend
+cd "$(dirname "$0")/../Sources/Server/Frontend" || exit
 
 npm install
 npm run build
