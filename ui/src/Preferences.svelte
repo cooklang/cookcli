06b6de68cc7ac6031b905467653f38a71fb7c7f6
leonbeon <leonch@live.de>
2023-12-31 14:59:46 +0100

    Add Preferences navigation link Add checkbox to toggle showUnitsNextToIngredients in preferences

---
+++ ui/src/Preferences.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {ListGroup, ListGroupItem, TabContent, TabPane, Button} from "sveltestrap";
+
+    import {showUnitsNextToIngredients} from "./store.js";
+</script>
+
+<TabContent>
+    <TabPane tabId="preferences" tab="Preferences" active>
+    <!-- add checkbox input to toggle showUnitsNextToIngredients store -->
+    <label class="my-3" for="show-units-next-to-ingredients">
+        <input type="checkbox" id="show-units-next-to-ingredients" bind:checked={$showUnitsNextToIngredients}>
+        <span>Show units next to ingredients in recipes</span>
+    </label>
+</TabPane>
+
+
+</TabContent>
